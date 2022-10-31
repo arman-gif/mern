98db8ba522c7ab6d40ff1135c13099795fabe115
Armanbabu12 <armanhossaincse3@gmail.com>
2022-10-31 22:46:02 +0600

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbaar from './componets/navbar';
+import Newnav from './componets/newnavbar/Newnav';
+import Maincomp from './componets/home/Maincomp';
+
+function App() {
+  return (
+    <>
+      <Navbaar />
+      <Newnav />
+      <Maincomp />
+    </>
+  );
+}
+
+export default App;
---
+++ componets/newnavbar/Newnav.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./newnav.css";
+
+const Newnav = () => {
+  return (
+    <div className='new_nav'>
+        <div className='nav_data'>
+            <div className='left_data'>
+                <p>All</p>
+                <p>Best Sellers</p>
+                <p>Pages</p>
+                <p>Fashion</p>
+                <p>Mobile</p>
+                <p>Shop</p>
+                <p>Customer Service</p>
+                <p>Today's Deals</p>
+                <p>Track Order</p>
+                <p>Need Help</p>
+            </div>
+            <div className='right_data'>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Newnav
